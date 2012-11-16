37dfcdb6d13509a1592468ef76d192c50ed15eb4
bengro <benjamin.groehbiel@gmail.com>
2012-11-16 20:48:55 +0100

    initial import

---
+++ src/main/java/ch/ethz/ir/dreamteam/naivebayesclassifier/App.java
@@ -0,0 +1,13 @@
+package ch.ethz.ir.dreamteam.naivebayesclassifier;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
